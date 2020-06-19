4ea2fff26751e6850ac9352e4c31724b0bba6665
Koutarou Tanaka <ktanaka@nzigen.com>
2020-06-19 10:10:32 +0900

    use `hs.keycodes.map`

--- function/eikanaEvent.lua
+++ function/eikanaEvent.lua
@@ -1,17 +1,12 @@
-local leftCmd = 0x37
-local rightCmd = 0x36
-local eisuu = 0x66
-local kana = 0x68
 local prevKeyCode
-
 local eisuuEvent = {}
 
 eisuuEvent.eventHandler = function(event)
   local isCmdKeyUp = not(event:getFlags()['cmd']) and event:getType() == hs.eventtap.event.types.flagsChanged
-  if isCmdKeyUp and prevKeyCode == leftCmd then
-    hs.eventtap.keyStroke({}, eisuu)
-  elseif isCmdKeyUp and prevKeyCode == rightCmd then
-    hs.eventtap.keyStroke({}, kana)
+  if isCmdKeyUp and prevKeyCode == hs.keycodes.map['cmd'] then
+    hs.eventtap.keyStroke({}, hs.keycodes.map['eisu'])
+  elseif isCmdKeyUp and prevKeyCode == hs.keycodes.map['rightcmd'] then
+    hs.eventtap.keyStroke({}, hs.keycodes.map['kana'])
   end
 
   prevKeyCode = event:getKeyCode()
