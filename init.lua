40fb7929bb74f4d24d379430fcc590c4e1967f93
Koutarou Tanaka <ktanaka@nzigen.com>
2020-06-19 10:09:46 +0900

    split configuration

--- init.lua
+++ init.lua
@@ -1,11 +1,1 @@
-local eikanaEvent = require('eikanaEvent')
-
-eikana = hs.eventtap.new(
-  {
-    hs.eventtap.event.types.keyDown,
-    hs.eventtap.event.types.flagsChanged
-  },
-  eikanaEvent.eventHandler
-)
-
-eikana:start()
+dofile(hs.configdir .. '/config/event.lua')
