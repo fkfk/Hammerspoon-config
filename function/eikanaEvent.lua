2feca5ef4c0ac95d41b59ac4b1b0be03248ae1f1
Koutarou Tanaka <ktanaka@nzigen.com>
2020-06-24 14:49:18 +0900

    set keyStroke delay

--- function/eikanaEvent.lua
+++ function/eikanaEvent.lua
@@ -4,9 +4,9 @@ local eisuuEvent = {}
 eisuuEvent.eventHandler = function(event)
   local isCmdKeyUp = not(event:getFlags()['cmd']) and event:getType() == hs.eventtap.event.types.flagsChanged
   if isCmdKeyUp and prevKeyCode == hs.keycodes.map['cmd'] then
-    hs.eventtap.keyStroke({}, hs.keycodes.map['eisu'])
+    hs.eventtap.keyStroke({}, hs.keycodes.map['eisu'], 1000)
   elseif isCmdKeyUp and prevKeyCode == hs.keycodes.map['rightcmd'] then
-    hs.eventtap.keyStroke({}, hs.keycodes.map['kana'])
+    hs.eventtap.keyStroke({}, hs.keycodes.map['kana'], 1000)
   end
 
   prevKeyCode = event:getKeyCode()
